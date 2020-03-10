2ba8b1a47c545cf6ad87f3e58f47fe17b3a5a806
Holger Benl <hbenl@evandor.de>
2020-03-10 20:19:20 +0100

    try to run out/test/runTest.js in a child process

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import { fork } from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,11 +15,25 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.runTests', () => {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		const workspacePath = vscode.workspace.workspaceFolders![0].uri.fsPath;
+
+		console.log('Creating child process');
+
+		const cp = fork(
+			path.resolve(workspacePath, 'out/test/runTest.js'),
+			[],
+			{
+//				execPath: '/usr/bin/node',
+				execArgv: [],
+				stdio: 'pipe'
+			}
+		);
+
+		cp.stdout?.on('data', data => console.log(data.toString()));
+		cp.stderr?.on('data', data => console.log(data.toString()));
 	});
 
 	context.subscriptions.push(disposable);
